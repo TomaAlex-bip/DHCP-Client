d008f24150fad060736b78fe45e03f8df3df1abe
Toma Alexandru <toma.alex.xd@gmail.com>
2021-12-03 08:55:03 +0200

    added separate threads for send and receive

--- client/NetworkInterface.py
+++ client/NetworkInterface.py
@@ -4,56 +4,63 @@ import select
 import threading
 
 
+serverPort = 20001
+clientPort = 8888
+serverAddress = "127.0.0.1"
+
+running = False
+
+
 class NetworkInterface:
 
-    def __init__(self, sport, dport, dip):
-
-        self.__s_port = sport
-        self.__d_port = dport
-        self.__d_ip = dip
-
+    def __init__(self):
         # Creare socket UDP
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-        self.__socket.bind(('0.0.0.0', int(self.__s_port)))
+        self.__socket.bind(('0.0.0.0', clientPort))
 
-        self.__running = True
+        # creare de threaduri separate pentru trimitere si pentru primire de date
+        self.__receive_thread = threading.Thread(target=self.__receive_function)
+        self.__send_thread = threading.Thread(target=self.__send_function)
 
-        # Creare thread pentru receptie de date
-        self.__receive_thread = threading.Thread(target=self.__receive_fct())
+        print('UDP Client created')
 
 
     def start(self):
+        global running
         try:
+            running = True
             self.__receive_thread.start()
+            self.__send_thread.start()
         except:
-            print("Eroare la pornirea thread‐ului")
+            print("Eroare la pornirea threadului")
             sys.exit()
 
-        while True:
-            try:
-                data = input("Trimite: ")
-                self.__socket.sendto(bytes(data, encoding="ascii"), (self.__d_ip, int(self.__d_port)))
-            except KeyboardInterrupt:
-                self.__running = False
-                print("Waiting for the thread to close...")
-                self.__receive_thread.join()
-                break
 
-
-    def __receive_fct(self):
+    def __receive_function(self):
         contor = 0
-        while self.__running:
+        while running:
             # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
             # Stabilim un timeout de 1 secunda
             r, _, _ = select.select([self.__socket], [], [], 1)
             if not r:
                 contor = contor + 1
+                # print(contor)
             else:
                 data, address = self.__socket.recvfrom(1024)
-                print("S-a receptionat ", str(data), " de la ", address)
-                print("Contor= ", contor)
+                print("\nS-a receptionat ", str(data), " de la serverul ", address)
+                print("contor = ", contor, "\nTrimite mesaj catre server: ")
 
 
-
+    def __send_function(self):
+        global running
+        while True:
+            try:
+                data = input("Trimite mesaj catre server: ")
+                self.__socket.sendto(str.encode(data), (serverAddress, serverPort))
+            except KeyboardInterrupt:
+                running = False
+                self.__receive_thread.join()
+                print("Closing thread...")
+                break
 
--- client/main.py
+++ client/main.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 import Interface
-import NetworkInterface
+from NetworkInterface import *
 
 
 
@@ -11,9 +11,21 @@ if __name__ == '__main__':
 
     gui = Interface.Interface(800, 600)
 
-    gui.run_interface()
+    # gui.run_interface()
 
-    # udp = NetworkInterface.NetworkInterface(nu stiu :) )
+    client = NetworkInterface()
+    client.start()
+
+
+# ponturi
+#
+# trebuie sa folosim codificari la nivel de pachet
+# cand le compunem trebuie sa avem grija la network order
+# stivele de comm folosesc big endian
+# pentru partea de impachetare ar fi bine sa folosim modulul struct (pack si unpack)
+# functia pack functioneaza astfel:
+# primeste ca prim parametru un specificator de format, apoi sirul de octeti
+
 
 
 
